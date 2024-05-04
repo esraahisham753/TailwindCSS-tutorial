c86f7e71baf7b84f7c65a5aef412ebc88a27f790
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-04 14:07:55 +0000

    Add design system markup

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -5,9 +5,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="parent h-screen bg-slate-900 text-white">
-      <div className="container bg-red-500">
-       <h1>Hello</h1>
+    <div>
+      <h1>This is a title</h1>
+      <h2>This is a subtitle</h2>
+      <p>This is a paragraph</p>
+      <a href="#">This is an anchor</a>
+      <div>
+        <button>This is a button</button>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="checkbox" id="checkbox" name="checkbox"/>
+        <label htmlFor="checkbox">Checkbox</label>
       </div>
     </div>
   );
