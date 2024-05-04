7d41098da8ca56a674878c2d3fecb603260f5964
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-04 16:28:27 +0000

    Add global layer classes

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
       <h1>This is a title</h1>
-      <h2>This is a subtitle</h2>
-      <p>This is a paragraph</p>
+      <h2 className="text-xl font-semibold mb-1">This is a subtitle</h2>
+      <p className="text-base">This is a paragraph</p>
       <a href="#">This is an anchor</a>
       <div>
         <button>This is a button</button>
@@ -20,6 +17,14 @@ export default function Home() {
         <input type="checkbox" id="checkbox" name="checkbox"/>
         <label htmlFor="checkbox">Checkbox</label>
       </div>
+      <div>
+        <select name="options" id="options">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </select>
+      </div>
+      <h1>This is a title</h1>
+      <h2 className="text-xl font-semibold mb-1">This is a subtitle</h2>
     </div>
   );
 }
